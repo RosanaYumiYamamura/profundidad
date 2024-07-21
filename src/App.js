d2e75814a284ae3e8574dbebaaf5e0fa2082cb12
RosanaYumiYamamura <rosanayumiyamamura1@gmail.com>
2024-07-21 06:52:41 -0300

    chaz

--- src/App.js
+++ src/App.js
@@ -1,22 +1,132 @@
 import Heading from './Heading.js';
 import Section from './Section.js';
+import { useState } from 'react';
+import { places } from './data.js';
+import { getImageUrl } from './utils.js';
+
+
+function FilterablePlaceTable({ places }) {
+  const [filterText, setFilterText] = useState('');
+
+
+  return (
+    <div>
+      <SearchBar 
+        filterText={filterText} 
+        onFilterTextChange={setFilterText} 
+        />
+      <PlacesTable 
+        places={places} 
+        filterText={filterText}
+         />
+         
+    </div>
+  );
+}
+
+function PlaceCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">
+        {category}
+      </th>
+    </tr>
+  );
+}
+
+function PlaceRow({ place }) {
+  const name = place.stocked ? place.name :
+    <span style={{ color: 'red' }}>
+      {place.name}
+    </span>;
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{place.país}</td>
+    </tr>
+  );
+}
+
+function PlacesTable({ places, filterText}) {
+  const rows = [];
+  let lastCategory = null;
+
+  places.forEach((place) => {
+    if (
+      place.name.toLowerCase().indexOf(
+        filterText.toLowerCase()
+      ) === -1
+    ) {
+      return;
+   
+    }
+    if (place.category !== lastCategory) {
+      rows.push(
+        <PlaceCategoryRow
+          category={place.category}
+          key={place.category} />
+      );
+    }
+    rows.push(
+      <PlaceRow
+        place={place}
+        key={place.name} />
+    );
+    lastCategory = place.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Lugar</th>
+          <th>País</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar({
+  filterText,
+  onFilterTextChange,
+}) {
+  return (
+    <form>
+      <input 
+        type="text" 
+        value={filterText} placeholder="Buscar..." 
+        onChange={(e) => onFilterTextChange(e.target.value)} />
+      
+    </form>
+  );
+}
+
 
 export default function ProfilePage() {
   return (
     <Section>
       <Heading>Viajando por Argentina</Heading>
+      <FilterablePlaceTable places={places} />
+      <Peperulo />
       <Post
         title="¡Hola viajero!"
         body="Lee sobre mis aventuras por Tucumán."
       />
+      
       <Post
         title="¡Hey familia!"
         body="He llegado a las Cataratas."
       />
+      
       <AllPosts />
     </Section>
   );
 }
+
+
 
 function AllPosts() {
   return (
@@ -24,6 +134,7 @@ function AllPosts() {
       <Heading>Publicaciones</Heading>
       <RecentPosts />
       <MainPosts />
+      
     </Section>
   );
 }
@@ -71,3 +182,60 @@ function Post({ title, body }) {
   );
 }
 
+function Peperulo(){
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+  return (
+    <>
+      <label>
+        <input
+          type="checkbox"
+          checked={isLarge}
+          onChange={e => {
+            setIsLarge(e.target.checked);
+          }}
+        />
+        Usa imágenes grandes
+      </label>
+      <hr />
+      <List imageSize={imageSize} />
+    </>
+  )
+}
+function List({ imageSize }) {
+  const listItems = places.map(place =>
+    <li key={place.id}>
+      <Place
+        place={place}
+        imageSize={imageSize}
+      />
+    </li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function Place({ place, imageSize }) {
+  return (
+    <>
+      <PlaceImage
+        place={place}
+        imageSize={imageSize}
+      />
+      <p>
+        <b>{place.name}</b>
+        {': ' + place.description}
+      </p>
+    </>
+  );
+}
+
+function PlaceImage({ place, imageSize }) {
+  return (
+    <img
+      src={getImageUrl(place)}
+      alt={place.name}
+      width={imageSize}
+      height={imageSize}
+    />
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const places = [{
+    id: 0,
+    name: 'Bariloche, Argentina',
+    description: 'San Carlos de Bariloche, comúnmente conocido como Bariloche, es una ciudad en la provincia de Río Negro, Argentina, situada en la región de la Patagonia. Rodeada por montañas y lagos, es un popular destino turístico durante todo el año, ofreciendo actividades como esquí en invierno y senderismo, ciclismo y deportes acuáticos en verano. Conocida también por su arquitectura de estilo suizo y su producción de chocolate artesanal, Bariloche combina belleza natural con un encanto cultural distintivo.',
+    imageId: 'ONpXGoU'
+  }, {
+    id: 1, 
+    name: 'Glaciar Perito Moreno en la Patagonia, Argentina',
+    description: 'Este épico hielo azul del Glaciar Perito Moreno en la Patagonia, Argentina.',
+    imageId: 's5X58m1'
+  }, {
+    id: 2, 
+    name: 'Pedro Martín Ureta, Argentina',
+    description: 'El Árbol de la Guitarra del Bosque de Pedro Martín Ureta, ubicado en Argentina, está compuesto por 7000 árboles plantados por un hombre en homenaje a su difunta esposa. Por el amor y versatilidad que su esposa tenía por la música y la guitarra, trabajó años para crear esta obra de arte en la naturaleza....',
+    imageId: 'DlLFS5R'
+  }, {
+    id: 3, 
+    name: 'Capital Federal, Argentina',
+    description: 'La Ciudad Autónoma de Buenos Aires, también conocida como Capital Federal, es la capital de Argentina. Situada en la costa este del país, a orillas del Río de la Plata, es el centro político, cultural y económico de Argentina. Con una rica historia que data de su fundación en 1580, la ciudad combina una arquitectura europea con influencias modernas. Buenos Aires es conocida por su vibrante vida cultural, incluyendo el tango, teatros, museos, y una animada escena gastronómica. La ciudad también alberga importantes instituciones gubernamentales, educativas y comerciales, y es un punto central para el turismo y el transporte en el país.',
+    imageId: 'T0x4Zyz'
+  }, {
+    id: 4, 
+    name: 'Zona San Telmo, Buenos Aires, Argentina',
+    description: 'San Telmo es uno de los barrios más antiguos y emblemáticos de Buenos Aires, conocido por sus calles empedradas, arquitectura colonial, y su animado mercado de antigüedades. Este barrio histórico es famoso por su vibrante escena cultural, con numerosos cafés, bares de tango y ferias de artesanías.',
+    imageId: 'uuXDDQD'
+  }, {
+    id: 5, 
+    name: 'Foz Iguazú, Misiones, Argentina',
+    description: 'Foz do Iguaçu es una ciudad brasileña ubicada en el estado de Paraná, famosa por albergar las impresionantes Cataratas del Iguazú, una de las maravillas naturales del mundo. Además de su belleza natural, la ciudad es un importante destino turístico y punto de encuentro de las fronteras de Brasil, Argentina y Paraguay.',
+    imageId: 'YyXofZQ'
+  }, {
+    id: 6,
+    name: 'Hornocal, Jujuy, Argentina',
+    description: 'Hornocal, en la provincia de Jujuy, Argentina, es conocido por su espectacular "Cerro de los 14 Colores," una formación montañosa que exhibe una impresionante variedad de colores debido a la sedimentación de distintos minerales. Este destino natural, situado cerca de la ciudad de Humahuaca, atrae a turistas por sus vistas panorámicas y su singular belleza geológica.',
+    imageId: 'n7zNrFv'
+  }];
+  
