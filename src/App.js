2128e78a1130bee9fcf1a8f4cadb88c9e61c8bde
RosanaYumiYamamura <rosanayumiyamamura1@gmail.com>
2024-07-21 02:47:03 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import Heading from './Heading.js';
+import Section from './Section.js';
 
-function App() {
+export default function ProfilePage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Section>
+      <Heading>Viajando por Argentina</Heading>
+      <Post
+        title="¡Hola viajero!"
+        body="Lee sobre mis aventuras por Tucumán."
+      />
+      <Post
+        title="¡Hey familia!"
+        body="He llegado a las Cataratas."
+      />
+      <AllPosts />
+    </Section>
   );
 }
 
-export default App;
+function AllPosts() {
+  return (
+    <Section>
+      <Heading>Publicaciones</Heading>
+      <RecentPosts />
+      <MainPosts />
+    </Section>
+  );
+}
+
+function MainPosts(){
+return (
+  <Section>
+      <Heading>Publicaciones Antiguas</Heading>
+      <Post
+        title="Sabores de Bariloche"
+        body="¡...esos chocolates con almendras!"
+      />
+      <Post
+        title="Buenos Aires a ritmo del chamamé"
+        body="¡Que sapucay!"
+      />
+    </Section>
+)
+}
+
+function RecentPosts() {
+  return (
+    <Section>
+      <Heading>Publicaciones recientes</Heading>
+      <Post
+        title="Las ballenas de Península de Valdés"
+        body="¡cuanta fauna y flora!"
+      />
+      <Post
+        title="Los Glaciares"
+        body="¡Que impresionante!"
+      />
+    </Section>
+  );
+}
+
+function Post({ title, body }) {
+  return (
+    <Section isFancy={true}>
+      <Heading>
+        {title}
+      </Heading>
+      <p><i>{body}</i></p>
+    </Section>
+  );
+}
+
